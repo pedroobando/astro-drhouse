1728ada8cb191fc8ca03ace196269cb720aa6632
pedroobando <pedroobando@hotmail.com>
2026-04-15 15:41:54 -0400

    vamos de nuevo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare(),
-
   vite: {
     plugins: [tailwindcss()],
   },
